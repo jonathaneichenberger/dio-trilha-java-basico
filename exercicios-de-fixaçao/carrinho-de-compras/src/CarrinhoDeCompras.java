c534f0788c0469af361b5df4b118eb73fab08906
Jonathan Eichenberger <djohnni@hotmail.com>
2024-10-18 11:43:53 -0300

    Update logic to calculate total price

--- exercicios-de-fixaçao/carrinho-de-compras/src/CarrinhoDeCompras.java
+++ exercicios-de-fixaçao/carrinho-de-compras/src/CarrinhoDeCompras.java
@@ -13,28 +13,39 @@ public class CarrinhoDeCompras {
     }
 
     public void removerItem(String nome) {
+        List<Item> itensParaRemover = new ArrayList<>();
         if (!listItems.isEmpty()) {
             for (int i = 0; i < listItems.size(); i++) {
                 if (listItems.get(i).getNome().equals(nome)) {
-                    System.out.println("Item: " + listItems.get(i).getNome() + " removido com sucesso!");
-                    listItems.remove(i);
+                    itensParaRemover.add(listItems.get(i));
                 }
             }
+            listItems.removeAll(itensParaRemover);
+        } else {
+            System.out.println("A lista está vazia!");
         }
     }
 
     public void calcularValorTotal() {
-        double total = 0;
-        for (Item item : listItems) {
-            total += item.getPreco() * item.getQuantidade();
+        double total = 0d;
+        if (!listItems.isEmpty()) {
+            for (Item item : listItems) {
+                total += item.getPreco() * item.getQuantidade();
+            }
+        } else {
+            throw new RuntimeException("A lista está vazia!");
         }
         System.out.println("Valor Total dos Produtos: R$" + String.format("%.2f", total));
     }
 
     public void exibirItens() {
-        for (int i = 0; i < listItems.size(); i++) {
-            System.out.println("Produto " + (i + 1) + ": " + listItems.get(i).toString());
-            System.out.println("------------------------------------------");
+        if (!listItems.isEmpty()) {
+            for (int i = 0; i < listItems.size(); i++) {
+                System.out.println("Produto " + (i + 1) + ": " + listItems.get(i).toString());
+                System.out.println("------------------------------------------");
+            }
+        } else {
+            System.out.println("A lista está vazia!");
         }
     }
 
@@ -45,16 +56,18 @@ public class CarrinhoDeCompras {
         carrinho.adicionarItem("TV", 500.00, 3);
         carrinho.adicionarItem("Notebook", 3500.00, 1);
         carrinho.adicionarItem("Celular", 1500.00, 2);
+        carrinho.adicionarItem("Celular", 1500.00, 2);
+        carrinho.adicionarItem("Celular", 1500.00, 2);
         carrinho.adicionarItem("Fone de Ouvido", 200.00, 4);
         carrinho.adicionarItem("Geladeira", 2500.00, 1);
         carrinho.adicionarItem("Cadeira Gamer", 700.00, 2);
 
         carrinho.exibirItens();
-
         carrinho.calcularValorTotal();
 
         carrinho.removerItem("Celular");
 
+        carrinho.exibirItens();
         carrinho.calcularValorTotal();
 
     }
