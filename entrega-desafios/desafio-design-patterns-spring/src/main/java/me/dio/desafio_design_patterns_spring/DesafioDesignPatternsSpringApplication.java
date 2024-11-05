b69ff9c4c6f78304a0209cb4d5ec1516f54b2626
Jonathan Eichenberger <djohnni@hotmail.com>
2024-11-05 12:21:18 -0300

    Primeiro commit - Inicialização do projeto

---
+++ entrega-desafios/desafio-design-patterns-spring/src/main/java/me/dio/desafio_design_patterns_spring/DesafioDesignPatternsSpringApplication.java
@@ -0,0 +1,13 @@
+package me.dio.desafio_design_patterns_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDesignPatternsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDesignPatternsSpringApplication.class, args);
+	}
+
+}
---
+++ entrega-desafios/desafio-design-patterns-spring/src/test/java/me/dio/desafio_design_patterns_spring/DesafioDesignPatternsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.desafio_design_patterns_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioDesignPatternsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
